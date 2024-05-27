22c17382577917b798b45cd22f1ae9f20b303e41
Gabriel Iope <gabriel.r.iope@gmail.com>
2024-05-27 08:51:51 -0300

    Correção regras de declaração de variável

--- ContaBanco/src/main/java/com/mycompany/contabanco/ContaBanco.java
+++ ContaBanco/src/main/java/com/mycompany/contabanco/ContaBanco.java
@@ -4,6 +4,10 @@
 
 package com.mycompany.contabanco;
 
+<<<<<<< HEAD
+=======
+import java.text.DecimalFormat;
+>>>>>>> 91c2762 (Conta Bancária teminal)
 import java.util.Scanner;
 /**
  *
@@ -12,13 +16,18 @@ import java.util.Scanner;
 public class ContaBanco {
 
     public static void main(String[] args) {
+<<<<<<< HEAD
         int numero;
+=======
+        String numero;
+>>>>>>> 91c2762 (Conta Bancária teminal)
         int agencia;
         String nome;
         float saldo;
         
         Scanner scan = new Scanner(System.in);
         
+<<<<<<< HEAD
         System.out.println("Por favor, digite o número da Agência!");
         agencia = scan.nextInt();
         scan.nextLine();
@@ -31,5 +40,19 @@ public class ContaBanco {
         saldo = scan.nextFloat();
         
         System.out.println("\nOlá " + nome + "! Obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo R$" + saldo + " já está disponível para saque.");
+=======
+        System.out.println("Por favor, digite o número da Nome!");
+        nome = scan.nextLine();
+        System.out.println("Por favor, digite o número da Agência!");
+        agencia = scan.nextInt();
+        scan.nextLine();
+        System.out.println("Por favor, digite o número da Número!");
+        numero = scan.nextLine();
+        System.out.println("Por favor, digite o número da Saldo!");
+        saldo = scan.nextFloat();
+        
+        System.out.print("\nOlá " + nome + "! Obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo R$");
+        System.out.printf("%.2f já está disponível para saque.", saldo);
+>>>>>>> 91c2762 (Conta Bancária teminal)
     }
 }
